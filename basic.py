74a62d094afb082f8e843f0e412341f436338ba4
NkosiV <Snuttie12@gmail.com>
2024-10-01 11:39:26 +0200

    Add another file

---
+++ basic.py
@@ -0,0 +1,20 @@
+# Create a program that asks the user for two numbers and 
+# prints their sum, difference, product, and quotient.
+
+# first number
+user = int(input("Give a number for sum: "))
+
+# second number
+user2 = int(input("Give a second number for sum: "))
+
+sum1 = user + user2
+print(sum1)
+
+sum2 = user - user2
+print(sum2)
+
+sum3 = user * user2
+print(sum3)
+
+sum4 = int(user / user2)
+print(sum4)
